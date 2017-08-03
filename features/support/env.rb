1bca94f7b6b911c1cce71e07fd7ded83250abc4a
Ian Dickinson <i.j.dickinson@gmail.com>
2017-08-03 17:24:03 +0100

    Migrate the test driver from Poltergeist to Selenium + Chrome Headless. Two Elda tests are not runnable under Selenium, since Selenium does not allow tests to read the response headers. These are currently commented out, and may be removed in future.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,34 +1,39 @@
 require 'capybara'
 require 'capybara/cucumber'
-#require 'capybara/mechanize/cucumber'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
-#require 'capybara-webkit'
-
+require 'selenium/webdriver'
 require 'byebug'
 
+# Pass the test host in from the environment
 HOST = ENV['TEST_HOST'] || '10.10.10.10'
-BASE = "http://#{HOST}"
+BASE = ''.freeze
+Capybara.app_host = "http://#{HOST}"
 
-Capybara.app = "make sure this isn't nil"
-#Capybara.javascript_driver = :selenium
-Capybara.register_driver :poltergeist do |app|
-#  Capybara::Poltergeist::Driver.new(app, :js_errors => false )
-  Capybara::Poltergeist::Driver.new(app, :timeout => 120 )
-  
+# Register a driver for visible Chrome using Selenium
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
-Capybara.default_driver    = :poltergeist
-Capybara.javascript_driver = :poltergeist
-#Capybara.javascript_driver = :webkit
 
+# Register a driver for headless Chrome using Selenium
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu] }
+  )
+
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :chrome,
+                                 desired_capabilities: capabilities)
+end
+
+# To see the Chrome window while tests are running, set this var to true
+see_visible_window_while_test_run = false
+driver = see_visible_window_while_test_run ? :chrome : :headless_chrome
+Capybara.default_driver    = driver
+Capybara.javascript_driver = driver
+
+# Enable adding a breakpoint into steps
 Then(/^breakpoint$/) do
-  byebug
+  byebug # rubocop:disable Lint/Debugger
   puts 'resuming ...'
 end
-
-##if Capybara.current_driver == :selenium
-#  require 'headless'
-#
-#  headless = Headless.new
-#  headless.start
-##end
