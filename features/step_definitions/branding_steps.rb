d5ff9c6e9526e4dee72bffa93532de34b8757abc
Ian Dickinson <i.j.dickinson@gmail.com>
2020-06-10 17:34:08 +0100

    Remove Cucumber features that are now covered by MiniTest specs

--- features/step_definitions/branding_steps.rb
+++
@@ -1,9 +0,0 @@
-
-When(/^she retrieves an elda page$/) do
-  visit "#{PROTO}//#{HOST}/anything"
-end
-
-Then(/^it should have an HMG banner$/) do
-  background_image = page.evaluate_script('$("nav.site").css("background-image")')
-  expect(background_image).to match(/url\(.*hmg-banner.png/)
-end
